989f2bee6d6a6b530f1f69854bee98024ab2947c
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-28 21:49:21 +0200

    moved handlers to seperate js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -15,6 +15,7 @@ const user= 'Lethabo';
 
 console.log (user);
 
+import { subtractHandler, addHandler,  } from './handlers.js';
 import { hello as helloA } from './code-a.js';
 import { hello as helloB} from './code-b.js';
 console.log (helloA, helloB);
@@ -29,31 +30,5 @@ const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
 
-
-const subtractHandler = () => {
-    const newValue = parseInt(number.value) - STEP_AMOUNT
-    number.value = newValue
-
-    if (add.disabled === true) {
-        add.disabled = false
-    }
-
-    if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
-    }
-}
-
-const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT
-    number.value = newValue
-
-    if (subtract.disabled === true) {
-        subtract.disabled = false
-    }
-
-    if (newValue >= MAX_NUMBER) {
-        add.disabled = true
-    }
-}
 subtract.addEventListener ('click', subtractHandler)
 add.addEventListener ('click', addHandler)
