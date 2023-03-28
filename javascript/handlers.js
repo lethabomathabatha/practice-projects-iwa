a1444a6dd746d8630f839b7eff1be00321bda2db
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-28 21:47:20 +0200

    handler export

---
+++ javascript/handlers.js
@@ -0,0 +1,28 @@
+
+export const subtractHandler = () => {
+    const newValue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newValue
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
+}
+
+export const addHandler = () => {
+    const newValue = parseInt(number.value) + STEP_AMOUNT
+    number.value = newValue
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+}
+
+
