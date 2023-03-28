cd1e8599833284ee7e3d28090fd95ec63b42c192
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-28 09:35:12 +0200

    first commit

---
+++ javascript/scripts.js
@@ -0,0 +1,55 @@
+
+
+
+//This is an online comment
+
+/*And this here,
+is a multi-line
+comment*/
+
+const id = 453523
+
+/**This is the assigned user. The user id is saved in: {@link id} */ 
+const user= 'Lethabo'
+
+
+
+
+console.log (user)
+
+const MAX_NUMBER = 15
+const MIN_NUMBER = -5
+const STEP_AMOUNT = 5; 
+
+
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+const subtractHandler = () => {
+    const newValue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newValue
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
+}
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) + STEP_AMOUNT
+    number.value = newValue
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+}
+subtract.addEventListener ('click', subtractHandler)
+add.addEventListener ('click', addHandler)
