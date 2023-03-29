45f40894fffb41d5370329a6de6276b75009765a
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-29 21:49:43 +0200

    intro to js variables

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,122 @@
 
+function createPassword() {
+    secretHiddenPassword = '2otb732gh24f0'
+    console.log("Your password is: " + secretHiddenPassword)
+  }
+  
+  function doSomethingElseNotRelatedToPassword () {
+      console.log("I have access to the password in here too: " + secretHiddenPassword)
+  }
+  
+  createPassword() 
+  // Outputs: Your password is: 2otb732gh24f0
+  
+  doSomethingElseNotRelatedToPassword() 
+  // Outputs: I have access to the password in here too: 2otb732gh24f0
+
+
+
+let value = (number) => {
+    let value = 43;
+    console.log("number is: " + number);
+  }
+
+
+/*
+let value = function (number) {
+    console.log("number is: " + number);
+  }
+  
+  // Several lines of code wrapping the above, making it hard to notice the declaration
+
+  // Uncaught SyntaxError: Identifier 'value' has already been declared
+*/
+
+
+
+
+
+/*
+console.log(one)
+console.log(two)
+
+var one = 'Magic, but unpredictable' 
+// undefined
+
+let two = 'Not magic, but predictable' 
+//  Uncaught ReferenceError: Cannot access 'two' before initialization
+*/
+
+console.log(value)
+getName()
+
+console.log(value)
+var value = 'John'
+
+function getName () {
+  value = 'Mary'
+}
+
+
+/*const exampleA = 21;
+let exampleB = 51;
+
+console.log(exampleB);
+
+exampleB = 1;
+console.log(exampleB);
+
+if (true) {
+    exampleB = 8;
+    console.log(exampleB);
+}
+
+if (true) {
+    console.log(13);
+
+    if (true) {
+    
+        console.log(2);
+
+
+        if (false) {
+            
+            console.log(3);
+        }
+    }
+}
+/*
+const test1 = () => {
+    console.log(value);
+}
+
+const test2 = () => {
+    console.log(value);
+}
+
+test1();
+test2();
+*/
+
+
+
+const valueA = 123;
+const valueB= 10;
+const valueC = valueA / valueB
+
+
+console.log(valueA);
+console.log( valueB);
+console.log(valueA + valueB);
+console.log(valueC);
+
+
+
+
+
+
+
+
 
 
 //This is an online comment
