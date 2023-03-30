d40770dcafcb8ca8c5bae3ec3219f038d8ba0c89
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-30 16:09:59 +0200

    added fcc exercises js

---
+++ javascript/freecodecamp-excercises.js
@@ -0,0 +1,21 @@
+const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+const badStr = 'Finn responds, "Let\'s go!"';
+
+console.log (goodStr);
+console.log (badStr);
+
+//indentations, tab, new line
+const myStr = "FirstLine \n\t\\SecondLine \nThirdLine"; // Change this line
+
+console.log (myStr)
+
+//Concatenating Strings with Plus Operator
+const concatenate= 'My name is Alan,' + ' I concatenate.';
+
+console.log (concatenate)
+
+//Concatenating Strings with the Plus Equals Operator.  This can be very helpful to break a long string over several lines.
+let ourStr = "I come first. ";
+ourStr += "I come second.";
+
+console.log (ourStr)
