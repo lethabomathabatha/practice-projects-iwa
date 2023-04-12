8f7b670ddbfe518771981c134ad067d08465cb39
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-12 09:09:12 +0200

    added CSSOM eg code

--- javascript/cssom-example-script.js
+++ javascript/cssom-example-script.js
@@ -1,50 +1,7 @@
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
+// get the elements by ID and class
+const heading = document.getElementById("heading");
+const text = document.querySelector(".text");
 
-const number = document.querySelector('[data-keys="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
-
-const updateColor = () => {
-    subtract.style.background = 'red';
-    add.style.background = 'red';
-};
-
-const subtractHandler = () => {
-    const newValue = parseInt(number.value) - STEP_AMOUNT;
-    number.value = newValue;
-
-    if (add.disabled === true) {
-        add.disabled = false;
-    }
-
-    if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true;
-    }
-};
-
-const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT;
-    number.value = newValue;
-
-    if (subtract.disabled === true) {
-        subtract.disabled = false;
-    }
-
-    if (newValue >= MAX_NUMBER) {
-        add.disabled = true;
-    }
-
-    updateColor();
-};
-
-subtract.addEventListener('click', subtractHandler);
-add.addEventListener('click', addHandler);
-
-
-
-import { subtractHandler, addHandler} from './handlers.js';
-import { hello as helloA } from './code-a.js';
-import { hello as helloB} from './code-b.js';
-console.log (helloA, helloB);
+// change the styles using CSSOM
+heading.style.color = "red";
+text.style.fontSize = "24px";
