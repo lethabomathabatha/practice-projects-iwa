d678847ae91679321989d4021357129bf636207f
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-12 08:51:47 +0200

    added style object lecture js

--- javascript/style-objects.js
+++ javascript/style-objects.js
@@ -1,53 +1,50 @@
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5; 
+const STEP_AMOUNT = 5;
 
-const elements = {
-    number: document.querySelector('[data-key="number"]'),
-    subtract: document.querySelector('[data-key="subtract"]'),
-    add: document.querySelector('[data-key="add"]')
-}
+const number = document.querySelector('[data-keys="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
 
 const updateColor = () => {
-    subtract.style.background = 'red'
-    add.style.background = 'red'
-}
-
-import { subtractHandler, addHandler,  } from './handlers.js';
-import { hello as helloA } from './code-a.js';
-import { hello as helloB} from './code-b.js';
-console.log (helloA, helloB);
-const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
-
-
-
+    subtract.style.background = 'red';
+    add.style.background = 'red';
+};
 
 const subtractHandler = () => {
-    const newValue = parseInt(number.value) - STEP_AMOUNT
-    number.value = newValue
+    const newValue = parseInt(number.value) - STEP_AMOUNT;
+    number.value = newValue;
 
     if (add.disabled === true) {
-        add.disabled = false
+        add.disabled = false;
     }
 
     if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
+        subtract.disabled = true;
     }
-}
+};
 
 const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT
-    number.value = newValue
+    const newValue = parseInt(number.value) + STEP_AMOUNT;
+    number.value = newValue;
 
     if (subtract.disabled === true) {
-        subtract.disabled = false
+        subtract.disabled = false;
     }
 
     if (newValue >= MAX_NUMBER) {
-        add.disabled = true
+        add.disabled = true;
     }
-}
-subtract.addEventListener ('click', subtractHandler)
-add.addEventListener ('click', addHandler)
-add.addEventListener ('click', addHandler)
+
+    updateColor();
+};
+
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click', addHandler);
+
+
+
+import { subtractHandler, addHandler} from './handlers.js';
+import { hello as helloA } from './code-a.js';
+import { hello as helloB} from './code-b.js';
+console.log (helloA, helloB);
