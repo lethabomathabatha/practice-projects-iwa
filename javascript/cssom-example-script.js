9fa3f7beaefbbd234f8198f6bbc41a6dda9ac197
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-12 09:28:51 +0200

    added DOM styling

--- javascript/cssom-example-script.js
+++ javascript/cssom-example-script.js
@@ -1,7 +1,12 @@
 // get the elements by ID and class
 const heading = document.getElementById("heading");
 const text = document.querySelector(".text");
+const hello = document.querySelector(".hello");
 
 // change the styles using CSSOM
 heading.style.color = "red";
 text.style.fontSize = "24px";
+
+// change the styles using DOM
+hello.style.fontSize = "40px";
+hello.style.color = "gold";
