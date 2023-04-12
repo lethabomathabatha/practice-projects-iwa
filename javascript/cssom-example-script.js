d3a25bb73a2978df2bac59830a7dbdae503506d6
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-12 09:32:06 +0200

    added comment

--- javascript/cssom-example-script.js
+++ javascript/cssom-example-script.js
@@ -7,6 +7,11 @@ const hello = document.querySelector(".hello");
 heading.style.color = "red";
 text.style.fontSize = "24px";
 
+
 // change the styles using DOM
 hello.style.fontSize = "40px";
 hello.style.color = "gold";
+
+// Although there is virtually no difference between the CSSOM and DOM methods in this specific example,
+// "changing the style of an element using the DOM directly will modify the inline styles of the element, 
+// which can override any styles defined in an external CSS file or stylesheet.
