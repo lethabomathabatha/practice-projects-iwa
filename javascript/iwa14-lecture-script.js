adec74147d04284425d1bcf9fa0a26be6c7a7106
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-14 09:59:12 +0200

    added iwa14 lecture js

--- javascript/iwa14-lecture-script.js
+++ javascript/iwa14-lecture-script.js
@@ -1,195 +1,93 @@
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
+// Function Expressions
 
-const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
+// what happens if we move the console.logs up before they are even created?
+// we get the error: cannot access before initialization
+// console.log(example1())
+// console.log(example2())
+// console.log(example3())
+// console.log(example4())
+
+// However, console.logging the last example actually logs. Why?
+// Hoisting. Cool, but rather skip this practice.
+console.log(example4())
+
+// Function-overloading and hoisting e.g:
+// console.log(example3())
+// the result of this is example 4 not being accessible, as it is overridden
+// an alert, for example, can be overridden if a function called 'alert' was used
 
 
-/*
-// function 1: subtractHandler
-const subtractHandler = () => {
-    const newValue = parseInt(number.value) - STEP_AMOUNT;
-    number.value = newValue;
 
-    if (add.disabled) {
-        add.disabled = false;
-    }
-
-    if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true;
-    }
-};
-
-// function 2: addHandler
-const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT;
-    number.value = newValue;
-
-    if (subtract.disabled) {
-        subtract.disabled = false;
-    }
-
-    if (newValue >= MAX_NUMBER) {
-        add.disabled = true;
-    }
-
-    updateColor();
-};
-
-subtract.addEventListener('click', subtractHandler);
-add.addEventListener('click', addHandler);
-*/
-
-// Functions lecture:
-
-// declaring a function, and calling it
+// multi-line, arrow function expression
 const example1 = () => {
-    console.log('Hey There')
-    console.log("How're you")
-    console.log('?')
-    console.log(Math.random)
+    return 123
 }
+console.log(example1())
 
-// example1()
-// example1()
-// example1()
-// example1()
 
-const example2 = example1
-const example3 = example1
-const example4 = example3
 
-example1()
-example2()
-example3()
+// Traditional Function Expression
+// if it all fits on a single line, eliminate curly brackets and "return"
+const example2 = () => 4 * 5
+console.log(example2())
 
-const customLog = () => {
-    const text = "Hello world"
-    const upperText = text.toUpperCase()
-    console.log(upperText)
+
+// different method, using function keyword
+const example3 = function () {
+    return 5334
 }
+console.log(example3())
 
-customLog()
-customLog()
-
-
-// you can assign the function 'text' when you call it to console
-const upperCaseLog = (text) => {
-    const upperText = text.toUpperCase()
-    console.log(upperText)
+// Function Declaration
+// you give the function a name
+function example4 () {
+    return 94
 }
-
-upperCaseLog("Lethabo")
-upperCaseLog("Mathabatha")
+console.log(example4())
 
 
-// 
+// using 'this'
+// re-binding 'this' to parent object
+// "in the code below, 'value' is a property of the 'counter' object, 
+// so when we use 'this.value', it refers to the 'value' property of the object."
+// removing 'this' would make 'value' a variable with a global scope, instead of 
+// being the property of the 'counter' object
+const counter = {
+    value: 120,
 
-/**
- * Logs a string to the console. The value passed will
- * be randomly converted to uppercase or lowercase!
- * @param {string} text - The text value to transform
- */
+    add: function () {
+        this.value = this.value + 1
+    },
 
-
-// you can assign the function 'text' when you call it to console
-const randomCaseLog = (text) => {
-    const isUpperCase = Math.random() >= 0.5
-    const method = isUpperCase ? 'toUpperCase' : 'toLowerCase'
-    const result = text[method]()
-    console.log(result)
-}
-
-parseInt('sfrgd')
-
-console.log(randomCaseLog("Lethabo"))
-console.log(randomCaseLog("Mathabatha"))
-console.log(randomCaseLog("26"))
-
-
-
-
-/**
- * @param {"uppercase"|"lowercase"} [format="uppercase"] - how to transform
- * the case. If no value is provided then one will be selected randomly.
- */
-
-
-const changeCase = (text, format) => {
-    const formatMap = {
-        random: Math.random() >= 0.5 ? 'toUpperCase' : 'toLowerCase'
-        uppercase:'toUppercase',
-        lowercase:'lowercase',
+    minus: function () {
+        this.value = this.value - 1
     }
-    const method 
-    const method = method ? formatMap[method] : ()
-
-    const isUpper = Math.random() >= 0.5
-    const method = isUpper ? 'toUpperCase' : 'toLowerCase'
-    return text[method]()
+    
 }
 
-
-console.log(changeCase("Lethabo"))
-console.log(changeCase("Mathabatha"))
-console.log(changeCase("26"))
-
+counter.add.call(counter)
+counter.add.call(counter)
+counter.add.call(counter)
+counter.minus.call(counter)
+console.log(counter.value) 
 
 //
-const double = (value) => {
-    const result = value * 2
-    return result
+const counter1 = {
+    value: 120,
+    toAdd: [10, 15, 3],
+
+// the 'add' function loops through each number in the toAdd array 
+// and adds it to the value property of the counter1 object.
+     add: function () {
+        const handler = (item) =>{
+            this.value = this.value + item
+        }
+        this.toAdd.forEach(handler)
+        }
 }
+// item is a parameter of the handler function. The handler function
+// is called for each element of the toAdd array using the forEach method.
+console.log(counter1.value)
+counter1.add()
+console.log(counter1.value)
 
-console.log(double(15))
-console.log(randomCaseLog("Mathabatha"))
-
-
-
-
-
-
-
-
-
-
-
-/*
-import { subtractHandler, addHandler} from './handlers.js';
-import { hello as helloA } from './code-a.js';
-import { hello as helloB} from './code-b.js';
-console.log (helloA, helloB);
-*/
-
-
-/*
-const updateColor = () => {
-    subtract.style.background = 'red';
-    add.style.background = 'red';
-};
-*/
-
-
-// /**
-//  * @param {string} text
-//  * @param {"uppercase"|"lowercase"} [format="uppercase"]
-//  * @returns {string}
-//  */
-
-// const changeCase = (text, format) => {
-//     const formatMap = {
-//         random: Math.random() >= 0.5 ? 'toUpperCase' : 'toLowerCase'
-//         uppercase:'toUppercase',
-//         lowercase:'lowercase',
-//     }
-
-//     const key = format || 'random'
-//     const method = formatMap[key]
-//     return text[method]()
-// }
-
-// console.log(changeCase("Lethabo"))
-// console.log(changeCase("Mathabatha"))
-// console.log(changeCase("26"))
