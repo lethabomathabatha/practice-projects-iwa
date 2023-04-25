351da5d1160bec1b5213dd6b8fc6c22ef44f5958
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-25 13:04:47 +0200

    added events lecture code

---
+++ javascript/events-lecture-script.js
@@ -0,0 +1,52 @@
+// access the DOM:
+// The # is for id
+
+const example = document.querySelector('#example');
+/*
+// this is the event type
+example.onClick = () => { 
+    example.innerText = 'Clicked.' // the inner text of the element with id example will be changed to 'Clicked.'
+    example.disabled = true
+}
+*/
+
+
+
+example.addEventListener(
+    'click', // this is the event type
+    // when clicked, the code inside this arrow function will run
+    () => { 
+        example.innerText = 'Clicked.' // the inner text of the element with id example will be changed to 'Clicked.'
+        example.disabled = true;
+    });
+
+
+
+// To return different colors when the button is clicked:
+const backgrounds = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'purple',
+    'orange',
+    'pink',
+]
+
+const buttonList = document.querySelectorAll('button');
+const buttonsArray = Array.from(buttonList);
+
+const calcRandomColor = () => {
+
+}
+
+
+console.log(buttonsArray);
+
+const handler = () => {
+    console.log('Clicked!');
+}
+
+for (const button of buttonsArray) {
+    button.addEventListener('click', handler);
+}
