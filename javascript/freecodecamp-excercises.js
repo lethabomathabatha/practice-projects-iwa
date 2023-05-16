3df921e1f1696f2146280dcd0ae8d5adf1f002e8
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-05-16 16:26:20 +0200

    added Basic Data Structures exercises

--- javascript/freecodecamp-excercises.js
+++ javascript/freecodecamp-excercises.js
@@ -2210,3 +2210,81 @@ function myFunc() {
     console.log("Still going!");
   }
 }
+
+
+// BASIC DATA STRUCTURES
+
+// Use an Array to Store a Collection of Data
+// We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to 
+// the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+let yourArray = ["Tomorrow", 17, "May 2023", true, false] // Change this line
+
+// Access an Array's Contents Using Bracket Notation
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+let myArray5 = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray5[1] = "Beanie"
+// Only change code above this line
+console.log(myArray5);
+
+// Add Items to an Array with push() and unshift()
+// the push() method adds elements to the end of an array, and unshift() adds elements to the beginning.
+/* 
+  We have defined a function, mixedNumbers, which we are passing an array as an argument. Modify the function by using push() 
+  and unshift() to add 'I', 2, 'three' to the beginning of the array and 7, 'VIII', 9 to the end so that the returned array contains 
+  representations of the numbers 1-9 in order. 
+*/ 
+function mixedNumbers(arr) {
+  // Only change code below this line
+arr.unshift('I', 2, 'three')
+arr.push(7, 'VIII', 9)
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+// Remove Items from an Array with pop() and shift()
+// pop() removes an element from the end of an array, while shift() removes an element from the beginning.
+/* 
+  We have defined a function, popShift, which takes an array as an argument and returns a new array. Modify the function, using pop() 
+  and shift(), to remove the first and last elements of the argument array, and assign the removed elements to their corresponding 
+  variables, so that the returned array contains their values.
+*/
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+// Remove Items Using splice()
+// splice() allows us to remove any number of consecutive elements from anywhere in an array.
+// splice()'s first parameter represents the index on the array from which to begin removing elements, while the second parameter indicates the number of elements to delete. 
+
+// We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+const arr5 = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr5.splice(1,4)
+
+// Only change code above this line
+console.log(arr5);
+
+// Add Items Using splice()
+
+/*
+  We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() 
+  to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places. */
+function htmlColorNames(arr) {
+  // Only change code below this line
+arr.splice(0,2, 'DarkSalmon', 'BlanchedAlmond')
+  // Only change code above this line
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+
+
