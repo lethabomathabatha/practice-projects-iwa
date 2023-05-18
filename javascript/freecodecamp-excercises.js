87adf44de90df998e0c32e203c4099a7ddcf434a
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-05-18 15:11:18 +0200

    added Basic Algorithm Scripting exercises

--- javascript/freecodecamp-excercises.js
+++ javascript/freecodecamp-excercises.js
@@ -2640,3 +2640,18 @@ console.log(getArrayOfUsers(users));
     return longest;
   }
   findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+  
+  // Return Largest Numbers in Arrays
+  // Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+  function largestOfFour(arr) {
+    let largest = [];
+    for (let i = 0; i < arr.length; i++) {
+      largest.push(Math.max(...arr[i]))
+    }
+    return largest;
+  }
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [-1000, -1001, -857, -1]]));
+
+  // Confirm the Ending
+  
